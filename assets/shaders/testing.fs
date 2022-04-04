a0a3aa9fd988d68fcdc408006ece1b0cc0de26d5
haroonsyed <haroonaminsyed@gmail.com>
2022-04-04 16:36:17 -0400

    Added more assets used in testing the expanded texture system.

--- assets/shaders/testing.fs
+++ assets/shaders/testing.fs
@@ -6,6 +6,7 @@ in vec3 transformedNorm;
 out vec4 FragColor;
 
 uniform vec3 lightPos;
+uniform mat4 view;
 
 uniform bool diffuseMapExists;
 uniform bool normalMapExists;
@@ -28,7 +29,7 @@ void main()
 
   // Calculate vectors needed for lighting
   vec3 normal = normalize(transformedNorm);
-  vec3 viewDir = normalize(transformedPos);
+  vec3 viewDir = normalize(transformedPos - vec3(view));
   vec3 toLight = normalize(lightPos-transformedPos);
   vec3 h = normalize((-1*viewDir) + toLight);
 
@@ -38,6 +39,8 @@ void main()
 
   float diffuse = max(dot(normal,toLight),0);
   float specular = max(pow(dot(normal,h), phongExp),0);
+
+  vec4 specularColor = vec4(1.0);
   
-  FragColor = (ambient + diffuse) * diffuseColor + specularStrength * specular * vec4(1.0);
+  FragColor = (ambient + diffuse) * diffuseColor + specularStrength * specular * specularColor;
 };
---
+++ assets/shaders/testing.vs
@@ -0,0 +1,23 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTex;
+layout (location = 2) in vec3 aNorm;
+
+out vec3 transformedPos;
+out vec2 uvCoord;
+out vec3 transformedNorm;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+   transformedPos = vec3(model * vec4(aPos,1));
+   transformedNorm = vec3(model * vec4(aNorm,0.0));
+   uvCoord = aTex;
+
+   // Calculate Position
+   gl_Position = projection * view * model * vec4(aPos,1);
+
+};
