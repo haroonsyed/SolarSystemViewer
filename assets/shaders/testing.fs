26d425c89fd98e060e71d4c7b9bbcfea067328bd
haroonsyed <haroonaminsyed@gmail.com>
2022-04-05 09:19:14 -0400

    Changed model to modelView. Takes less data to send to gpu and certian calculations for specular are easier.

--- assets/shaders/testing.fs
+++ assets/shaders/testing.fs
@@ -1,12 +1,11 @@
 #version 330 core
 in vec3 transformedPos;
-in vec2 uvCoord;
 in vec3 transformedNorm;
+in vec2 uvCoord;
 
 out vec4 FragColor;
 
 uniform vec3 lightPos;
-uniform mat4 view;
 
 uniform bool diffuseMapExists;
 uniform bool normalMapExists;
@@ -29,18 +28,18 @@ void main()
 
   // Calculate vectors needed for lighting
   vec3 normal = normalize(transformedNorm);
-  vec3 viewDir = normalize(transformedPos - vec3(view));
   vec3 toLight = normalize(lightPos-transformedPos);
+  vec3 viewDir = normalize(transformedPos);
   vec3 h = normalize((-1*viewDir) + toLight);
 
   float ambient = 0.1;
-  float specularStrength = 1;
-  float phongExp = 100;
+  float specularStrength = 0.5f;
+  float phongExp = 100.0f;
 
   float diffuse = max(dot(normal,toLight),0);
-  float specular = max(pow(dot(normal,h), phongExp),0);
+  float specular = pow(max(dot(normal,h), 0),phongExp);
 
-  vec4 specularColor = vec4(1.0);
+  vec4 specularColor = specularMapExists ? specularMapData : vec4(1.0);
   
   FragColor = (ambient + diffuse) * diffuseColor + specularStrength * specular * specularColor;
 };
